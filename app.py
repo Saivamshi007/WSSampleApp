234de630d80e8de44cc274df91d8cb53bc2a2b7c
Sunny <dsaivamshi123@gmail.com>
2020-01-01 09:47:53 +0530

    saivamshi

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from selenium import webdriver
 
 
 import spacy
-nlp=spacy.load('en_core_web_sm')
+nlp=spacy.load('en')
 from spacy import displacy
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
